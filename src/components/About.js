4b4d44784af25ae0abd21363dc1bf784632f8d88
aishwaryamehar10 <aishwaryamehar10@gmail.com>
2023-03-17 18:54:15 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useContext, useEffect } from 'react';
+
+
+const About = () => {
+
+    return (
+        <div>
+            This is about page
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/context/notes/noteContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const noteContext = createContext(); //Syntax to create context
+
+export default noteContext;
